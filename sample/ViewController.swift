989247b9266f7126411848f17d51f1947a8d8165
Reza Shirazian <reza.shirazian@gmail.com>
2017-06-23 14:58:19 -0700

    inital

---
+++ sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Reza Shirazian on 6/23/17.
+//  Copyright © 2017 YourMechanic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
